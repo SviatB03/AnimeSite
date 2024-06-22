3da02a3665fdb4bc4597e96f5f80c4a738627aa6
user <user@WIN-T9NTGAQGALP>
2024-06-23 01:00:30 +0300

    add admin category list

---
+++ AnimeSite/Controllers/CategoryController.cs
@@ -0,0 +1,87 @@
+﻿using AnimeSite.Entity;
+using AnimeSite.Repository.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnimeSite.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly IGenreRepository _genreRepository;
+
+        public CategoryController(IGenreRepository genreRepository)
+        {
+            _genreRepository = genreRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var genres = await _genreRepository.GetAllAsync();
+            return View(genres);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Genre genre)
+        {
+            if (ModelState.IsValid)
+            {
+                await _genreRepository.AddAsync(genre);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(genre);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var genre = await _genreRepository.GetByIdAsync(id);
+            if (genre == null)
+            {
+                return NotFound();
+            }
+            return View(genre);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Genre genre)
+        {
+            if (id != genre.GenreId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                await _genreRepository.UpdateAsync(genre);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(genre);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var genre = await _genreRepository.GetByIdAsync(id);
+            if (genre == null)
+            {
+                return NotFound();
+            }
+            return View(genre);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _genreRepository.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
