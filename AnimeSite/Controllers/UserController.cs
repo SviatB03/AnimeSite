dbfd381be828c901bfb0c0249465015175eaa318
user <user@WIN-T9NTGAQGALP>
2024-06-23 02:19:29 +0300

    add userlist view

---
+++ AnimeSite/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using AnimeSite.Repository.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnimeSite.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userRepository.GetAllAsync();
+            return View(users);
+        }
+    }
+}
