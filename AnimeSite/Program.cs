bf45be3bf5dd2ed90551e87954e5c85540fa750c
user <user@WIN-T9NTGAQGALP>
2024-06-22 21:52:25 +0300

    add repos to DI container

--- AnimeSite/Program.cs
+++ AnimeSite/Program.cs
@@ -1,7 +1,25 @@
+using MySql.Data.MySqlClient;
+using System.Data;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using AnimeSite.Repository.Interfaces;
+using AnimeSite.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Add MySQL connection
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddScoped<IDbConnection>((sp) => new MySqlConnection(connectionString));
+
+// Register repositories
+builder.Services.AddScoped<IAnimeRepository, AnimeRepository>();
+builder.Services.AddScoped<IGenreRepository, GenreRepository>();
+builder.Services.AddScoped<IReleaseScheduleRepository, ReleaseScheduleRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserAnimeTrackingRepository, UserAnimeTrackingRepository>();
 
 var app = builder.Build();
 
